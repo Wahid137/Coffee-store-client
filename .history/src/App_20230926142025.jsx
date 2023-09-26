63cfad5a1b4b5189489c70bb11ce30306fb44748
wahid-ahmed <wahidahmedshanto@gmail.com>
2023-09-26 14:23:28 +0600

    intial setup

---
+++ .history/src/App_20230926142025.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="color: red-500">Hello world</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20230926142037.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-80000">Hello world</h1>
+    </>
+  );
+}
+
+export default App;
